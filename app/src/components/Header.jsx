da3187c0cefd65a248b71c57ca9b18aac8a69363
Betacraft <hoangkimc9@gmail.com>
2024-06-30 15:21:05 +0700

    build: add login button

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ThemeController from "src/components/ThemeController";
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
-    <div className="border-b-2 py-6">
+    <div className="border-b-2 px-5 py-6 lg:px-0">
       <div className="container mx-auto flex items-center justify-between">
         <Link to="/" className="text-3xl font-bold tracking-tight">
           Logo.com
         </Link>
-        <ThemeController />
+        <div className="flex items-center justify-center gap-3">
+          <ThemeController />
+          <button className="btn" onClick={() => navigate("/login")}>
+            Đăng Nhập
+            <FontAwesomeIcon icon={faRightToBracket} className="text-xl" />
+          </button>
+        </div>
       </div>
     </div>
   );
