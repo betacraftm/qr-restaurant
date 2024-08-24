38ab3ac634cbb817b703c4f057bca54c4e6e7839
Betacraft <hoangkimc9@gmail.com>
2024-08-24 23:30:01 +0700

    build: add responsive for header

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,18 +1,75 @@
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
+import { useState } from "react";
 
 export default function Header() {
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header className="flex items-center justify-between bg-stone-300 p-2">
-      <div className="cursor-pointer" onClick={() => navigate("/")}>
-        <img
-          src="horizontal-logo.png"
-          alt="logo"
-          className="h-10 object-contain"
-        />
+    <div>
+      <header className="navbar shadow-md">
+        <div className="navbar-start">
+          <div className="cursor-pointer" onClick={() => navigate("/")}>
+            <img
+              src="horizontal-logo.png"
+              alt="logo"
+              className="h-10 object-contain"
+            />
+          </div>
+        </div>
+        <div className="navbar-center hidden md:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>How To Use</a>
+            </li>
+            <li>
+              <a>Features</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="btn hidden md:inline-flex">
+            <Link to={"/sign-in"}>Sign In</Link>
+          </div>
+          <span
+            className="rounded-md bg-red-500 p-2 text-2xl text-white md:hidden"
+            onClick={() => setShowMenu((prev) => !prev)}
+          >
+            {showMenu ? <IoCloseSharp /> : <GiHamburgerMenu />}
+          </span>
+        </div>
+      </header>
+      <div className={showMenu ? "block" : "hidden"}>
+        <ul className="menu w-full rounded-none bg-base-200 p-0 shadow-md">
+          <li
+            className="rounded-none border-b border-slate-300"
+            onClick={() => setShowMenu((prev) => !prev)}
+          >
+            <a>About</a>
+          </li>
+          <li
+            className="rounded-none border-b border-slate-300"
+            onClick={() => setShowMenu((prev) => !prev)}
+          >
+            <a>How To Use</a>
+          </li>
+          <li
+            className="rounded-none border-b border-slate-300"
+            onClick={() => setShowMenu((prev) => !prev)}
+          >
+            <a>Features</a>
+          </li>
+          <li onClick={() => setShowMenu((prev) => !prev)}>
+            <Link to={"/sign-in"}>Sign In</Link>
+          </li>
+        </ul>
       </div>
-      <button className="btn">Sign In</button>
-    </header>
+    </div>
   );
 }
